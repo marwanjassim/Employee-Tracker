0fa9e02c7779e4291da81cd515d043f4ffd72195
marwan jassim <marwanjassim87@gmail.com>
2020-01-18 00:08:24 -0800

    use on delete cascade for tables

--- Schema.sql
+++ Schema.sql
@@ -16,6 +16,7 @@ CREATE TABLE roles(
     CONSTRAINT fk_department_id
     FOREIGN KEY (department_id)
       REFERENCES departments(id)
+      ON DELETE CASCADE
 );
 CREATE TABLE employees(
     id INT NOT NULL AUTO_INCREMENT,
@@ -26,8 +27,10 @@ CREATE TABLE employees(
     PRIMARY KEY (id),
     CONSTRAINT fk_role_id
     FOREIGN KEY (role_id)
-      REFERENCES roles(id),
+      REFERENCES roles(id)
+      ON DELETE CASCADE,
     CONSTRAINT fk_manager_id
     FOREIGN KEY (manager_id)
       REFERENCES employees(id)
+      ON DELETE CASCADE
 );
