a850f3badeb75f2f20c6f1da962f41f1044291a1
marwan jassim <marwanjassim87@gmail.com>
2020-01-17 18:31:25 -0800

    completed all the tables in schema.sql and ran it on sql

--- Schema.sql
+++ Schema.sql
@@ -2,6 +2,21 @@ DROP DATABASE IF EXISTS employee_tracker;
 CREATE DATABASE employee_tracker;
 USE employee_tracker;
 
+CREATE TABLE departments(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE roles(
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR (30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_department_id
+    FOREIGN KEY (department_id)
+      REFERENCES departments(id)
+);
 CREATE TABLE employees(
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
@@ -14,6 +29,5 @@ CREATE TABLE employees(
       REFERENCES roles(id),
     CONSTRAINT fk_manager_id
     FOREIGN KEY (manager_id)
-      REFERENCES employees(id),
-)
-CREATE TABLE
+      REFERENCES employees(id)
+);
